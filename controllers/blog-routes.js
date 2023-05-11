34d9a2b98d54b2f1e720d647ecf5edb65f39e518
blackshane <122633142+blackshane@users.noreply.github.com>
2023-05-11 16:19:04 -0500

    created db

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -2,7 +2,8 @@
 
  // GET Route to Display the Homepage with Handlebars
  router.get('/', async (req, res) => {
-    res.render('homepage')
+    res.render('homepage'//, {whatever model is called}
+    )
  });
 
  module.exports = router;
