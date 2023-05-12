68afa900105f383c519f2944ed690ee058fc789a
blackshane <122633142+blackshane@users.noreply.github.com>
2023-05-12 17:11:15 -0500

    fixed links to stylesheets and update server.js file

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
-// Import dependencies
-const express = require('express');
-const exphbs = require('express-handlebars');
 const path = require('path');
-const hbs = exphbs.create({});
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-// Sets up the Express App
+const sequelize = require('./config/connection');
+
+// Imports Sequelize Store to store session data
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set Handlebars as the default template engine.
+const hbs = exphbs.create({ helpers });
+
+// Set up the session
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({ // THIS IS
+    db: sequelize             // IMPORTANT 
+  })
+};
+
+// Initiate express to start session using the "sess" object
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/blog-routes'));
 
-// Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log('Server listening on: http://localhost:' + PORT);
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
